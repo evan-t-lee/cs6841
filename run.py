20ee1989a1166ebf807d59841ccb953cd3eee4fa
Evan Lee <evanlee2010@hotmail.com>
2020-04-15 09:44:35 +1000

    updated user customisability and interface

--- run.py
+++ run.py
@@ -6,9 +6,19 @@ from joblib import load
 # user imports
 from shared import parse_features
 
+show_email = input('Would you like the input email to be shown (y/n)? ')
+show_email = True if show_email == 'y' else False
+
 nb_model = load('nb_model.joblib')
 
 input_matrix = parse_features(['in.txt'])
 input_result = nb_model.predict(input_matrix)
 
-print('Phishy' if input_result else 'Not phishy')
+if show_email:
+	email = open('in.txt').read()
+	print('\n------------------------------')
+	print(email)
+	print('------------------------------')
+
+email_result = 'phishy' if input_result else 'not phishy'
+print(f'\nAccording to our analysis, it is likely that this email is {email_result}.')
