7986ffba0f212cb7cdfa64cd7d8a57d80ad2f085
Evan Lee <evanlee2010@hotmail.com>
2020-04-15 12:31:15 +1000

    updated algorithm

--- parse.py
+++ parse.py
@@ -5,41 +5,38 @@ from collections import Counter
 from numpy import zeros
 from joblib import dump, load
 
-MAX_KEYWORDS = 3000
+MAX_WORDS = 3000
 
 def collate_directory(directory):
     return [os.path.join(directory, filename) for filename in os.listdir(directory)]
 
+def normalise_words(line):
+    return re.sub(r'[^a-zA-Z ]+', '', line.lower().strip())
+
 def count_frequency(files, has_custom):
-    all_words = []
+    words = []
     for filename in files:
         with open(filename) as text:
             for i, line in enumerate(text):
                 if i < 2: # content starts from 3rd line
                     continue
-                sentence = re.sub(r'[^a-zA-Z ]+', '', line.strip())
-                sentence = re.sub(r'\b[a-zA-Z]\b', '', sentence)
-                all_words += sentence.split()
+                words += normalise_words(line).split()
 
-    if has_custom:
-        custom_words = [(line.strip(), MAX_KEYWORDS)for line in open('keywords.txt')]
-        word_amount = MAX_KEYWORDS - len(custom_words)
-        word_freq = custom_words + Counter(all_words).most_common(word_amount)
-    else:
-        word_freq = Counter(all_words).most_common(MAX_KEYWORDS)
+    custom_words = [(line.strip(), MAX_WORDS) for line in open('custom.txt')] if has_custom else []
+    word_freq = custom_words + Counter(words).most_common(MAX_WORDS - len(custom_words))
     dump(word_freq, 'word_freq.joblib')
 
 def parse_features(files):
     word_freq = load('word_freq.joblib')
-    features_matrix = zeros((len(files), MAX_KEYWORDS))
+    features_matrix = zeros((len(files), MAX_WORDS))
     for i, filename in enumerate(files):
         with open(filename) as text:
             for j, line in enumerate(text):
                 if j < 2: # content starts from 3rd line
                     continue
-                words = line.strip().split()
+                words = line.split()
                 for word in words:
-                    for k, count in enumerate(word_freq):
-                        if count[0] == word:
+                    for k, word_count in enumerate(word_freq):
+                        if word_count[0] == word:
                             features_matrix[i, k] = words.count(word)
     return features_matrix
